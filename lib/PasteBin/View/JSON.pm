dbe415a8e1267e7f44e56125dfd70499e7c621c5
Edward Prendergast <info@madduck.co.uk>
2010-09-23 23:39:43 +0100

    Overrode json rendering... things are looking up but the DateTime objects inside the recordset are now causing problems

--- lib/PasteBin/View/JSON.pm
+++ lib/PasteBin/View/JSON.pm
@@ -2,6 +2,7 @@ package PasteBin::View::JSON;
 
 use strict;
 use base 'Catalyst::View::JSON';
+use JSON::XS ();
 
 =head1 NAME
 
@@ -25,6 +26,13 @@ This library is free software, you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+sub encode_json {
+      my($self, $c, $data) = @_;
+      my $encoder = JSON::XS->new->ascii->pretty->allow_nonref;
+	  #$encoder->allow_blessed( [ 1 ] );
+	  $encoder->convert_blessed( [1] );
+      $encoder->encode($data);
+}
 
 __PACKAGE__->config( { expose_stash => [ qw/result/ ] } );
 
